b66b8b4d584d3765bee6dcd2cd5f9ab204089a27
weblerson <zonejr4@gmail.com>
2023-07-11 19:43:32 -0400

    changed year property name on db column using Column annotation

--- src/main/java/com/lerson/dslist/entities/Game.java
+++ src/main/java/com/lerson/dslist/entities/Game.java
@@ -12,6 +12,8 @@ public class Game {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+
+    @Column(name = "game_year")
     private Integer year;
     private String genre;
     private String platforms;
