508c61de02fc2c27defc119b62b1cab0baf61fab
weblerson <zonejr4@gmail.com>
2023-07-12 14:25:08 -0400

    created GameListService with findAllMethod

---
+++ src/main/java/com/lerson/dslist/services/GameListService.java
@@ -0,0 +1,25 @@
+package com.lerson.dslist.services;
+
+import com.lerson.dslist.dto.GameListDTO;
+import com.lerson.dslist.entities.GameList;
+import com.lerson.dslist.repositories.GameListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class GameListService {
+
+    @Autowired
+    private GameListRepository gameListRepository;
+
+    @Transactional(readOnly = true)
+    public List<GameListDTO> findAll() {
+        List<GameList> result = this.gameListRepository.findAll();
+
+        return result.stream().map(GameListDTO::new).collect(Collectors.toList());
+    }
+}
