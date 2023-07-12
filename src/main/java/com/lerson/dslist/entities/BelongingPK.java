b71a59bbfb29df9de3b31fa7de9630d2724e2073
weblerson <zonejr4@gmail.com>
2023-07-12 13:46:20 -0400

    created BelongingPK embeddable class to multiple pk

---
+++ src/main/java/com/lerson/dslist/entities/BelongingPK.java
@@ -0,0 +1,57 @@
+package com.lerson.dslist.entities;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.util.Objects;
+
+@Embeddable  // Serve para dizer que essa classe representa dois campos na tabela relacional
+public class BelongingPK {
+
+    @ManyToOne
+    @JoinColumn(name = "name_id")  // Isso vai ser mapeado para dois campos chave estrangeira na tabela
+    private Game game;
+
+    @ManyToOne
+    @JoinColumn(name = "list_id")
+    private GameList gameList;
+
+    public BelongingPK() {
+
+    }
+
+    public BelongingPK(Game game, GameList gameList) {
+        this.game = game;
+        this.gameList = gameList;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public GameList getGameList() {
+        return gameList;
+    }
+
+    public void setGameList(GameList gameList) {
+        this.gameList = gameList;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BelongingPK that = (BelongingPK) o;
+        return Objects.equals(game, that.game) && Objects.equals(gameList, that.gameList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(game, gameList);
+    }
+}
