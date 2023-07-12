c1e561fcea887a9b86fc8c209921bc2dd83c1c9c
weblerson <zonejr4@gmail.com>
2023-07-12 14:09:23 -0400

    created findById service on GameService

--- src/main/java/com/lerson/dslist/services/GameService.java
+++ src/main/java/com/lerson/dslist/services/GameService.java
@@ -1,5 +1,6 @@
 package com.lerson.dslist.services;
 
+import com.lerson.dslist.dto.GameDTO;
 import com.lerson.dslist.dto.GameMinDTO;
 import com.lerson.dslist.entities.Game;
 import com.lerson.dslist.repositories.GameRepository;
@@ -14,6 +15,12 @@ public class GameService {
 
     @Autowired
     private GameRepository gameRepository;
+
+    public GameDTO findById(Long id) {
+        Game result = this.gameRepository.findById(id).get();
+
+        return new GameDTO(result);
+    }
 
     public List<GameMinDTO> findAll() {
 
