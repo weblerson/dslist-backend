84656530a86f5d5fd23a904f10692f9b720b1556
weblerson <zonejr4@gmail.com>
2023-07-12 15:00:50 -0400

    created searchByList method on GameRepository interface

--- src/main/java/com/lerson/dslist/repositories/GameRepository.java
+++ src/main/java/com/lerson/dslist/repositories/GameRepository.java
@@ -1,8 +1,23 @@
 package com.lerson.dslist.repositories;
 
 import com.lerson.dslist.entities.Game;
+import com.lerson.dslist.projections.GameMinProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
 
+    // nativeQuery = true serve para ativar a consulta SQL padrão
+    // O resultado da consulta é uma interface. No Spring, uma projection
+    @Query(nativeQuery = true, value = """
+		SELECT tb_game.id, tb_game.title, tb_game.game_year AS `year`, tb_game.img_url AS imgUrl,
+		tb_game.short_description AS shortDescription, tb_belonging.position
+		FROM tb_game
+		INNER JOIN tb_belonging ON tb_game.id = tb_belonging.game_id
+		WHERE tb_belonging.list_id = :listId
+		ORDER BY tb_belonging.position
+			""")
+	List<GameMinProjection> searchByList(Long listId);
 }
