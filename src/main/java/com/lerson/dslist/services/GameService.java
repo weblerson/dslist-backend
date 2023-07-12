b56511ad9ace51881823b6767733f2f2ee25af83
weblerson <zonejr4@gmail.com>
2023-07-12 14:11:56 -0400

    made service's methods transactional and readonly

--- src/main/java/com/lerson/dslist/services/GameService.java
+++ src/main/java/com/lerson/dslist/services/GameService.java
@@ -6,6 +6,7 @@ import com.lerson.dslist.entities.Game;
 import com.lerson.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,12 +17,14 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)  // Garante que a operação com DB aconteça obedecendo aos princípios das transações
     public GameDTO findById(Long id) {
         Game result = this.gameRepository.findById(id).get();
 
         return new GameDTO(result);
     }
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
 
         List<Game> result = this.gameRepository.findAll();
