2abad3ff6e377403d972acb9484481b182d85a7d
weblerson <zonejr4@gmail.com>
2023-07-12 14:14:35 -0400

    created findById method on GameController with path variable  id

--- src/main/java/com/lerson/dslist/controllers/GameController.java
+++ src/main/java/com/lerson/dslist/controllers/GameController.java
@@ -1,9 +1,11 @@
 package com.lerson.dslist.controllers;
 
+import com.lerson.dslist.dto.GameDTO;
 import com.lerson.dslist.dto.GameMinDTO;
 import com.lerson.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +17,13 @@ public class GameController {
 
     @Autowired
     private GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {  // Torna o ID como uma variável de url
+        GameDTO result = this.gameService.findById(id);
+
+        return result;
+    }
 
     @GetMapping
     public List<GameMinDTO> findAll() {
