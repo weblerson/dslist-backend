5bca8e3d2320ce48e7bff254d76d1fa9a5dd080d
weblerson <zonejr4@gmail.com>
2023-07-12 14:20:11 -0400

    renamed ListRepository to GameListRepository

--- src/main/java/com/lerson/dslist/repositories/GameListRepository.java
+++ src/main/java/com/lerson/dslist/repositories/GameListRepository.java
@@ -3,5 +3,5 @@ package com.lerson.dslist.repositories;
 import com.lerson.dslist.entities.GameList;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ListRepository extends JpaRepository<GameList, Long> {
+public interface GameListRepository extends JpaRepository<GameList, Long> {
 }
