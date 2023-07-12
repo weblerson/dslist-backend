66cbee1df331ac81f77901fda3b4fcab205b2559
weblerson <zonejr4@gmail.com>
2023-07-12 15:11:32 -0400

    created GameMinDTO constructor overload to handle GameMinProjection

--- src/main/java/com/lerson/dslist/dto/GameMinDTO.java
+++ src/main/java/com/lerson/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.lerson.dslist.dto;
 
 import com.lerson.dslist.entities.Game;
+import com.lerson.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 
@@ -20,6 +21,14 @@ public class GameMinDTO {
         this.year = entity.getYear();
         this.imgUrl = entity.getImgUrl();
         this.shortDescription = entity.getShortDescription();
+    }
+
+    public GameMinDTO(GameMinProjection projection) {
+        this.id = projection.getId();
+        this.title = projection.getTitle();
+        this.year = projection.getYear();
+        this.imgUrl = projection.getImgUrl();
+        this.shortDescription = projection.getShortDescription();
     }
 
     public Long getId() {
