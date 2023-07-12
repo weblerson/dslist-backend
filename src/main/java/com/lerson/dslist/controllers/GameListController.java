ff4b1830c87f7e45e96529f1c842a20aaec68bcc
weblerson <zonejr4@gmail.com>
2023-07-12 14:28:49 -0400

    corrected some annotations on GameListController

--- src/main/java/com/lerson/dslist/controllers/GameListController.java
+++ src/main/java/com/lerson/dslist/controllers/GameListController.java
@@ -3,16 +3,20 @@ package com.lerson.dslist.controllers;
 import com.lerson.dslist.dto.GameListDTO;
 import com.lerson.dslist.services.GameListService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping(value = "/lists")
 public class GameListController {
 
     @Autowired
     private GameListService gameListService;
 
+    @GetMapping
     public List<GameListDTO> findAll() {
 
         return this.gameListService.findAll();
